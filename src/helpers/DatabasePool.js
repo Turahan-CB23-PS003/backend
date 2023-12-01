21c41ea23caad9ebdb6d057bb2468aef6e073702
Azhar Zaidan Fauzi <a.zaidan.f.azf616@gmail.com>
2023-12-01 18:55:38 +0700

    fix socketpath

--- src/helpers/DatabasePool.js
+++ src/helpers/DatabasePool.js
@@ -2,12 +2,12 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+  socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
 });
 
 module.exports = pool;
